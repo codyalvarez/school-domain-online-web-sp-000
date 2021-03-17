0e80f38191f3b27ae687c3ed826e5198b6465022
Cody Alvarez <cody_alvarez19@outlook.com>
2021-03-17 00:51:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,14 @@
 class School
 
-  def initialize(school_name)
-    @school_name = school_name
+  def initialize(school_name)  #we initialize a school with its name here
+    @school_name = school_name  #we set our instance variable to school_name
     @roster = {}
   end
 
   def roster
     @roster
   end
+
+  def add_student
+    
 end
